d3c61355ddeafc9d36b192da8f4bfe68898dca79
eierapfel <eierapfel.125@gmail.com>
2020-03-23 00:20:20 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt update 
 sudo apt install ros-melodic-kobuki-* -y
 sudo apt install ros-melodic-ecl-streams -y
-sudo apt install ros-melodic-joy
+sudo apt install ros-melodic-joy -y
+sudo apt install ros-melodic-joint-state-publisher* -y
