3b7e9fe00c69cdd80920ccf10a4ec9bcc648f42a
agurakakenai <manabukoide@soka.gr.jp>
2021-10-11 18:06:01 +0900

    added libgazebo_ros_kobuki.so

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt update 
 sudo apt install ros-melodic-kobuki-* -y
 sudo apt install ros-melodic-ecl-streams -y
 sudo apt install ros-melodic-joy -y
 sudo apt install ros-melodic-joint-state-publisher* -y
+sudo cp ~/catkin_ws/src/sobit_common/turtlebot2/turtlebot_simulator/turtlebot_gazebo/libgazebo_ros_kobuki.so /opt/ros/melodic/lib
 # 関係ない
 sudo apt install ros-melodic-pcl-*
 sudo apt install ros-melodic-openni2-*
