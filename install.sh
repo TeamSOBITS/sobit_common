16a5aae5ca8b8cd5c5ad152a411d62fc68472afe
MrKeith99 <kvalentincardenas@hotmail.com>
2023-01-30 12:08:16 +0900

    update to install.sh and renew

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo "╔══╣ Install: SOBITS Common (STARTING) ╠══╗"
+
+
+sudo apt-get update
+sudo apt-get install -y \
+    ros-${ROS_DISTRO}-cv-bridge \
+    ros-${ROS_DISTRO}-geometry*\
+    ros-${ROS_DISTRO}-image-geometry
+
+# Install DynamixelSDK
+git clone -b noetic-devel https://github.com/TeamSOBITS/DynamixelSDK
+
+# Install TurtleBot2
+git clone https://github.com/TeamSOBITS/turtlebot2_on_noetic
+bash turtlebot2_on_noetic/install.sh
+
+# Link Kobuki library with gazebo (not necessary?)
+# sudo cp ~/catkin_ws/src/sobits_common/turtlebot2_on_noetic/turtlebot_simulator/turtlebot_gazebo/libgazebo_ros_kobuki.so /opt/ros/${ROS_DISTRO}/lib
+
+
+echo "╚══╣ Install: SOBITS Common (FINISHED) ╠══╝"
