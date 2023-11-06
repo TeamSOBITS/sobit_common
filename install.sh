ba99d8db742498b718ed3411c4f05921823d974c
MrKeith99 <kvalentincardenas@hotmail.com>
2023-11-06 16:28:37 +0900

    [UPD] new install.sh with kobuki/custom mobile base params

--- install.sh
+++ install.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
+# Select mobile base version
+MOBILE_BASE_LIST=("kobuki" "custom")
+MOBILE_BASE=$1
+MOBILE_BASE=${MOBILE_BASE//-}
 
-echo "╔══╣ Install: SOBITS Common (STARTING) ╠══╗"
 
-# 必要なパッケージをダウンロードする
+# Check if the mobile base is selected
+if [[ "${MOBILE_BASE_LIST[*]}" =~ (^|[[:space:]])"${MOBILE_BASE}"($|[[:space:]]) ]]; then
+    echo "╔══╣ Install: SOBITS Common with $MOBILE_BASE mobile base (STARTING) ╠══╗"
+else
+    echo "Please select one of the following mobile bases:"
+    echo ${MOBILE_BASE_LIST[*]}
+    echo "For example: bash install.sh --kobuki"
+    exit
+fi
+
+
+# Download and install dependencies
 sudo apt-get update
 sudo apt-get install -y \
-    ros-${ROS_DISTRO}-cv-bridge \
-    ros-${ROS_DISTRO}-geometry* \
-    ros-${ROS_DISTRO}-image-geometry \
-    ros-${ROS_DISTRO}-smach*
+    ros-${ROS_DISTRO}-controller_manager \
+    ros-${ROS_DISTRO}-hardware-interface \
+    ros-${ROS_DISTRO}-joint-limits-interface
 
 # Install DynamixelSDK
 git clone -b noetic-devel https://github.com/TeamSOBITS/DynamixelSDK
 
-# Install TurtleBot2
-git clone https://github.com/TeamSOBITS/turtlebot2_on_noetic
-cd turtlebot2_on_noetic
-bash install.sh
+if [[ "${MOBILE_BASE}" == "kobuki" ]]; then
+    # Install TurtleBot2
+    git clone https://github.com/TeamSOBITS/turtlebot2_on_noetic
+    cd turtlebot2_on_noetic
+    bash install.sh
 
-# Link Kobuki library with gazebo (not necessary?)
-# sudo cp ~/catkin_ws/src/sobit_common/turtlebot2_on_noetic/turtlebot_simulator/turtlebot_gazebo/libgazebo_ros_kobuki.so /opt/ros/${ROS_DISTRO}/lib
+    # Link Kobuki library with gazebo (not necessary?)
+    # sudo cp ~/catkin_ws/src/sobit_common/turtlebot2_on_noetic/turtlebot_simulator/turtlebot_gazebo/libgazebo_ros_kobuki.so /opt/ros/${ROS_DISTRO}/lib
 
 
 echo "╚══╣ Install: SOBITS Common (FINISHED) ╠══╝"
